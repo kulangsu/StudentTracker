c8a9823257b7fb7505ccf15a3be7015cd725aed4
Hiromi <hiromif630@gmail.com>
2015-05-18 11:54:58 -0700

    Added EditClass

---
+++ StudentTracker/Instructor/EditClass.aspx
@@ -0,0 +1,120 @@
+﻿<%@ Page Title="Create New Class" Language="C#" MasterPageFile="~/Instructor/Instructor.master" AutoEventWireup="true" CodeBehind="EditClass.aspx.cs" Inherits="StudentTracker.Instructor.EditClass" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <style> .ui-autocomplete { font-size:11px; text-align:left; } </style>
+       <script lang="javascript" type="text/javascript">
+            $(document).ready(function () {
+                //auto completed textbox when type
+                $('#<%=ClassName.ClientID%>').autocomplete({
+                source: function (request, response) {
+                    $.ajax({
+                        url: "../AjaxController.aspx/GetCourseList",
+                        data: "{ 'pre':'" + request.term + "'}",
+                        dataType: "json",
+                        type: "POST",
+                        contentType: "application/json; charset=utf-8",
+                        success: function (data) {
+                            response($.map(data.d, function (item) {
+                                return { value: item }
+                            }))
+                        },
+                        error: function (XMLHttpRequest, textStatus, errorThrown) {
+                            alert(textStatus);
+                        }
+                    });
+                }
+            });
+        });
+    </script>
+    <div class="form-horizontal">
+        <div class="row">
+            <div class="col-md-offset-3 col-md-8">
+                <h2><%: Title %></h2>
+                <h4>Create new class each quarter. Select quarter year then enter class name. 
+                    If your QuarterYear is not found in the dropdown list, it needz to be created first.</h4>
+            </div>
+        </div>
+    </div>
+    <div class="form-horizontal">
+        <div class="row">
+            <div class="col-md-offset-3 col-md-8">
+                <asp:ValidationSummary runat="server" CssClass="text-danger" />
+                <h4 class="text-danger">
+                    <asp:Literal runat="server" ID="ErrorMessage" />
+                </h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-group">
+                <asp:Label runat="server" AssociatedControlID="selectQuarterYear" CssClass="col-md-3 control-label">Quarter Year</asp:Label>
+                <div class="col-md-4">
+                    <asp:DropDownList ID="selectQuarterYear" runat="server" CssClass="form-control" AutoPostBack="True" OnSelectedIndexChanged="selectQuarterYear_SelectedIndexChanged">
+                    </asp:DropDownList>
+                </div>
+            </div>
+            <div class="form-group">
+                <asp:Label runat="server" AssociatedControlID="ClassName" CssClass="col-md-3 control-label">Class Name</asp:Label>
+                <div class="col-md-1">
+                    <asp:DropDownList ID="CourseArea" runat="server" CssClass="form-control" AutoPostBack="True" DataSourceID="SqlCoursePrefixList" DataTextField="PrefixName" DataValueField="PrefixID" OnSelectedIndexChanged="CourseArea_SelectedIndexChanged" Width="75px">
+                    </asp:DropDownList>
+                </div>
+                <div class="col-md-1">
+                    <asp:SqlDataSource ID="SqlCoursePrefixList" runat="server" ConnectionString="<%$ ConnectionStrings:dbStudentTracker %>" SelectCommand="SELECT * FROM [CoursePrefixs] ORDER BY [PrefixName]"></asp:SqlDataSource>
+                    <asp:DropDownList ID="CourseNumber" runat="server" CssClass="form-control" AutoPostBack="True" Width="75px"></asp:DropDownList>
+                </div>
+                <div class="col-md-2">                    
+                    <asp:DropDownList ID="CourseSection" runat="server" CssClass="form-control" AutoPostBack="True" Width="100px" DataSourceID="SqlCourseSectionList" DataTextField="Section" DataValueField="Section"></asp:DropDownList>
+                    <asp:SqlDataSource ID="SqlCourseSectionList" runat="server" ConnectionString="<%$ ConnectionStrings:dbStudentTracker %>" SelectCommand="SELECT * FROM [CourseSections] ORDER BY [Section]"></asp:SqlDataSource>
+                </div>
+                <div class="col-md-4">
+                    <asp:TextBox runat="server" ID="ClassName" CssClass="form-control cap_first_letter textboxAuto" />                     
+                    <asp:RequiredFieldValidator runat="server" ControlToValidate="ClassName"
+                        CssClass="text-danger" ErrorMessage="Class Name field is required." Display="Dynamic" />
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-md-offset-3 col-md-4">
+                <asp:Button ID="btnClassCreate" runat="server" OnClick="CreateClass_Click" Text="Save Class" CssClass="btn btn-primary" />
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-offset-3 col-md-6">
+            <h4 class="text-danger">
+                <asp:Literal runat="server" ID="ClassListMessage" />
+            </h4>
+        </div>
+        <div class="col-md-offset-2 col-md-9">
+            <h4>Courses Link To:
+                <asp:Literal runat="server" ID="FullName" /></h4>
+            <asp:GridView ID="GridViewInstructorClassList" runat="server" CssClass="table" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="CourseID" HeaderText="Course ID" InsertVisible="False" ReadOnly="True" SortExpression="CourseID" />
+                    <asp:BoundField DataField="Year" HeaderText="Year" SortExpression="Year" />
+                    <asp:BoundField DataField="Quarter" HeaderText="Quarter" SortExpression="Quarter" />
+                    <asp:BoundField DataField="CourseName" HeaderText="Course Name" SortExpression="CourseName" />
+                </Columns>
+                <EmptyDataTemplate>
+                    <h4 class="text-danger">No Class found from selected quarter.</h4>
+                </EmptyDataTemplate>
+            </asp:GridView>
+        </div>
+        <div class="col-md-offset-2 col-md-9">
+            <h4>All Courses below link to other Instructors:</h4>
+            <asp:GridView ID="GridViewClassList" runat="server" CssClass="table" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="CourseID" HeaderText="Course ID" SortExpression="CourseID" />
+                    <asp:BoundField DataField="FullName" HeaderText="Instructor" SortExpression="FullName" />
+                    <asp:BoundField DataField="Year" HeaderText="Year" SortExpression="Year" />
+                    <asp:BoundField DataField="Quarter" HeaderText="Quarter" SortExpression="Quarter" />
+                    <asp:BoundField DataField="CourseName" HeaderText="Course Name" SortExpression="CourseName" />
+                </Columns>
+                <EmptyDataTemplate>
+                    <h4 class="text-danger">No Class found from selected quarter.</h4>
+                </EmptyDataTemplate>                
+            </asp:GridView>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ StudentTracker/Instructor/EditClass.aspx.cs
@@ -0,0 +1,194 @@
+﻿using StudentTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace StudentTracker.Instructor
+{
+    public partial class EditClass : System.Web.UI.Page
+    {
+        //see IdentityModels.cs
+        StudentTrackerDBContext db = new StudentTrackerDBContext();
+
+        //see STLib.cs file
+        GetQuarter getQuarter = new GetQuarter();
+        RoleManager roleManager = new RoleManager();
+        CapFirstLetter capFirstLetter = new CapFirstLetter();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int yr = DateTime.Now.Year;
+            if (!IsPostBack)
+            {
+                //loading quarter year from database to gridview
+                var yrArr = new int[] { yr, yr + 1 };
+
+                //var query = from q in db.QuarterYears where (q => yrArr.Contains(q.Year)) orderby(q => q.);
+
+                var qrtYearList = db.QuarterYears
+                    .Where(c => yrArr.Contains(c.Year))
+                    .OrderByDescending(c => c.Year)
+                    .Select(i => new { _ID = i.ID, _QrtYr = i.Year + " - " + i.Quarter })
+                    .ToList();
+
+                selectQuarterYear.DataValueField = "_ID";
+                selectQuarterYear.DataTextField = "_QrtYr";
+                selectQuarterYear.DataSource = qrtYearList;
+                selectQuarterYear.DataBind();
+
+                if (qrtYearList.Count == 0)
+                {
+                    ErrorMessage.Text = "No QuarterYear found for curreent year or next year. Quarter Year need to create before can create class.";
+                    btnClassCreate.Enabled = false;
+                    ClassListMessage.Text = "";
+                }
+
+                //load Classes List that link to Instructor
+                LoadInstructorClassList(getQuarter.CurrentQuart());
+                LoadAllInstructorClassList(getQuarter.CurrentQuart());
+
+                //loading default course number
+                int BIT = 1;
+                LoadCourseNumber(BIT);
+
+                //get User full name
+                var manager = new UserManager<User>(new UserStore<User>(new StudentTrackerDBContext()));
+                var currentUser = manager.FindById(Context.User.Identity.GetUserId());
+                FullName.Text = currentUser.FirstName + ", "+currentUser.LastName;
+            }
+        }
+
+        //load All Classes List that link to ALL Instructors
+        //per Quarter Year selected
+        protected void LoadAllInstructorClassList(string qrt)
+        {
+            int yr = DateTime.Now.Year;
+            var yrArr = new int[] { yr, yr + 1 };
+            string userID = User.Identity.GetUserId();
+
+            //load all instructor userID
+            var users = roleManager.ReturnAllInstructor(); 
+
+            var CourseLists = db.UsersCourses
+                .Join(db.Courses, c => c.CourseId, cm => cm.ID, (c, cm) => new { c, cm })
+                .Join(db.QuarterYears, q => q.cm.QuarterYearID, qm => qm.ID, (q, qm) => new { q, qm })
+                .Where(w => w.qm.Quarter.Equals(qrt) && users.Contains(w.q.c.UserId) && !w.q.c.UserId.Equals(userID))
+                .OrderByDescending(q => q.q.cm.Name)
+                .Select(i => new {i.q.c.UserId, CourseID = i.q.cm.ID, CourseName = i.q.cm.Name, Year = i.qm.Year, Quarter = i.qm.Quarter });
+
+
+            var list = (from u in db.Users
+                        join a in CourseLists on u.Id equals a.UserId
+                        select new { a.CourseID, FullName = u.FirstName + " " + u.LastName, a.Year, a.Quarter, a.CourseName }
+                       ).ToList();
+
+            GridViewClassList.DataSource = list;
+            GridViewClassList.DataBind();
+        }
+
+        //load Classes List that link to Instructore
+        //per Quarter Year selected
+        protected void LoadInstructorClassList(string qrt)
+        {
+            int yr = DateTime.Now.Year;
+            var yrArr = new int[] { yr, yr + 1 };
+            string userID = User.Identity.GetUserId();
+            var CourseLists = db.UsersCourses
+                .Join(db.Courses, c => c.CourseId, cm => cm.ID, (c, cm) => new { c, cm })
+                .Join(db.QuarterYears, q => q.cm.QuarterYearID, qm => qm.ID, (q, qm) => new { q, qm })
+                .Where(w => w.qm.Quarter.Equals(qrt) && w.q.c.UserId.Equals(userID))
+                .OrderByDescending(q => q.q.cm.Name)
+                .Select(i => new { CourseID = i.q.cm.ID, CourseName = i.q.cm.Name, Year = i.qm.Year, Quarter = i.qm.Quarter })
+                .ToList();
+
+            GridViewInstructorClassList.DataSource = CourseLists;
+            GridViewInstructorClassList.DataBind();
+        }
+
+        protected void CreateClass_Click(object sender, EventArgs e)
+        {
+            ErrorMessage.Text = "";
+
+            string CoursePre = CourseArea.SelectedItem.Text;
+            string CourseNum = CourseNumber.SelectedItem.Text;
+            string courseName = ClassName.Text;
+            string CourseSec = CourseSection.SelectedItem.Text;
+
+            courseName = CoursePre + " " + CourseNum + " " + capFirstLetter.CapLetterString(ClassName.Text, ' ') + " " + CourseSec;
+            //quick check to see if Year & QuarterYear already exist
+            int qrtyrid = Convert.ToInt32(selectQuarterYear.SelectedValue);
+            var quarteryear = db.Courses
+                              .Where(q => q.QuarterYearID == qrtyrid && q.Name.Equals(courseName))
+                              .ToList();
+
+            if (quarteryear.Count == 0)
+            {
+                //insert new quarteryear into database
+                var addClass = new Course
+                {
+                    QuarterYearID = Convert.ToInt32(selectQuarterYear.SelectedValue),
+                    Name = courseName
+                };
+                db.Courses.Add(addClass);
+                db.SaveChanges();
+                int classID = addClass.ID;
+                if (classID > 0)
+                {
+                    var addClassToIntructor = new UsersCourse
+                    {
+                        CourseId = classID,
+                        UserId = User.Identity.GetUserId()
+                    };
+                    db.UsersCourses.Add(addClassToIntructor);
+                    classID = db.SaveChanges();
+                }
+
+                if (classID > 0)
+                {
+                    ErrorMessage.Text += "<br>New Class created successful.";
+                    //load Classes List that link to Instructor
+                    LoadInstructorClassList(getQuarter.CurrentQuart());
+                    LoadAllInstructorClassList(getQuarter.CurrentQuart());
+                }
+                else
+                    ErrorMessage.Text += "System failed to insert new Class to database.";
+            }
+            else
+            {
+                ErrorMessage.Text = "Similar class entry already existed in database.";
+            }
+        }
+
+        protected void selectQuarterYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string qrt = selectQuarterYear.SelectedItem.Text;
+            string[] temp = qrt.Split('-');
+            LoadInstructorClassList(temp[1].Trim());
+            LoadAllInstructorClassList(temp[1].Trim());
+        }
+
+        protected void CourseArea_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int coursePrefix = Convert.ToInt32(CourseArea.SelectedValue);
+            LoadCourseNumber(coursePrefix);
+        }
+
+        protected void LoadCourseNumber(int num)
+        {           
+            var courseNumberList = db.CourseNumbers
+                .Where(c => c.PrefixID == num)
+                .OrderBy(c => c.Number)
+                .ToList();
+
+            CourseNumber.DataValueField = "Number";
+            CourseNumber.DataTextField = "Number";
+            CourseNumber.DataSource = courseNumberList;
+            CourseNumber.DataBind();
+        }
+    }
+}
