08d4fb29bf46cba55312d9ed17d0067c35a7bbe2
todd123178 <todd.taylor.tt@gmail.com>
2015-06-04 08:27:58 -0700

    auto generated feedback code can now be found in student class.  when a student uploads an assignment, a feedback document is instanly created and currently is just placed into the app_data folder.  we need to create a folder system.  this will help us make it easy for mikle to download and also seperate out the feedback that students are ready to view and also the template that has no addition feedback added.

--- StudentTracker/Student/StudentClass.aspx.cs
+++ StudentTracker/Student/StudentClass.aspx.cs
@@ -7,12 +7,19 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Configuration;
+using Novacode;
 
 namespace StudentTracker.Student
 {
     public partial class StudentClass : System.Web.UI.Page
     {
         StudentTrackerDBContext db = new StudentTrackerDBContext();
+        //gets the path of the local storage on the server
+        string FolderPath = ConfigurationManager.AppSettings["AppDataFolderPath"];
+       // name to be used in Insertion into student assignment files
+        string fileName;
+        
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -48,7 +55,7 @@ namespace StudentTracker.Student
             string user = User.Identity.GetUserId();
             int Assignment_ID = Convert.ToInt32(drpDwn_Assignment.SelectedValue);
             string file_name = System.IO.Path.GetFileName(FileUpload1.FileName);
-            FileUpload1.SaveAs(Server.MapPath("~/Files/") + file_name);
+            FileUpload1.SaveAs(Server.MapPath(FolderPath) + file_name);
 
             //insert new assignment into StudentAssignment table
             var addStudentAssignment = new StudentAssignment
@@ -77,12 +84,123 @@ namespace StudentTracker.Student
                 {
 
                     ErrorMessage.Text += "<br>You have successfully upload your assignment.";
+                    //to create the feedback and insert the infor into the tabels for tracking
+                    createFeedback();
+                    insertFeedbackToTable();
                 }
                 else
                     ErrorMessage.Text += "<br>System failed to upload your assignment.";
             }
             else
                 ErrorMessage.Text += "<br>System failed to upload your assignment.";
+
+        }
+        //this method inserts the information for the feedback document so it can be tracked and retrieved
+        public void insertFeedbackToTable(){
+            //get userID
+            string user = User.Identity.GetUserId();
+            int Assignment_ID = Convert.ToInt32(drpDwn_Assignment.SelectedValue);
+            //Adds the file uploaded into the student assignment table
+            var addStudentAssignment = new StudentAssignment
+            {
+                AssignmentID = Assignment_ID,
+                UserId = user
+            };
+            //adds and saves
+            db.StudentAssignments.Add(addStudentAssignment);
+            int StudentAssignment_ID = db.SaveChanges();
+            //this next line gets the Id of the resulting insertion from above.  this is then passed to the studentassignmentfile table
+            StudentAssignment_ID=addStudentAssignment.StudentAssignmentID;
+            if (StudentAssignment_ID > 0)
+            {
+                //insert new assignment into AssignmentFiles table
+                var addAssignmentFile = new AssignmentFile
+                {
+                    StudentAssignmentID = StudentAssignment_ID,
+                    FileName = fileName,
+                    UploadDate = System.DateTime.Now
+                };
+
+                db.AssignmentFiles.Add(addAssignmentFile);
+                int AssignmentFile_ID = db.SaveChanges();
+            }
+            
+        }
+        //will create a feedback document using the users info  from the file upload
+        private void createFeedback()
+        {
+                    
+            string user = User.Identity.GetUserId();
+            //gets the user name to display
+            var query =(from c in db.Users
+              where c.Id == user
+              select new { c.LastName, c.FirstName }).Single();
+            String stuName = query.FirstName + " " + query.LastName;
+            //gets assignment number to help 
+            int Assignment_ID = Convert.ToInt32(drpDwn_Assignment.SelectedValue);
+            string assignName = drpDwn_Assignment.SelectedItem.Text;
+            //uses attributes from the user and file to create a unique name for the file to be saved
+           fileName = Server.MapPath(FolderPath + "Feedback_" + assignName + "_" + stuName + "_" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".docx");
+            // Set up our paragraph contents:
+            string headerText = "Feedback Document for " + assignName;
+            string letterBodyText = DateTime.Now.ToShortDateString();
+            string StudentName =stuName + Environment.NewLine + Environment.NewLine;
+            //Main area for comments to be left.  for now we added a little bacon ipsum
+            string paraTwo = ""
+                + "PLEASE WRITE INDIVIDUAL COMMENTS HERE" + Environment.NewLine + Environment.NewLine
+                + "Bacon ipsum dolor amet cupim ball tip t-bone corned beef beef ribs drumstick doner "
+            + "andouille brisket short ribs ground round shoulder. Prosciutto pig ham ham hock, "
+            + "pork belly picanha chicken pastrami turducken capicola salami biltong hamburger. "
+            + "Filet mignon alcatra chicken, ham doner short ribs cupim t-bone tail chuck drumstick biltong salami porchetta capicola. "
+            + "Venison pastrami jerky leberkas pancetta tri-tip drumstick kevin landjaeger chuck meatloaf meatball capicola pork kielbasa. "
+            + "Meatloaf doner pork chop, porchetta alcatra chicken tongue flank pork belly beef kielbasa beef ribs chuck. "
+            + "Ground round boudin capicola, flank salami leberkas hamburger fatback shoulder tongue pork chop rump. "
+            + "Venison pork loin picanha swine prosciutto, pastrami cow t-bone landjaeger meatloaf."
+
+                + Environment.NewLine + Environment.NewLine + Environment.NewLine + Environment.NewLine + Environment.NewLine;
+
+            string paraThree = "GRADE GOES HERE";
+
+            // Title Formatting:
+            var titleFormat = new Formatting();
+            titleFormat.FontFamily = new System.Drawing.FontFamily("Arial Black");
+            titleFormat.Size = 18D;
+            titleFormat.Position = 12;
+
+            // Body Formatting
+            var paraFormat = new Formatting();
+            paraFormat.FontFamily = new System.Drawing.FontFamily("Calibri");
+            paraFormat.Size = 10D;
+            titleFormat.Position = 12;
+
+            // Grade Formatting
+            var gradeFormat = new Formatting();
+            titleFormat.FontFamily = new System.Drawing.FontFamily("Arial Black");
+            titleFormat.Size = 14D;
+            titleFormat.Position = 12;
+
+
+
+            // Create the document in memory:
+            var doc = DocX.Create(fileName);
+
+            // Insert each prargraph, with appropriate spacing and alignment:
+            Paragraph title = doc.InsertParagraph(headerText, false, titleFormat);
+            title.Alignment = Alignment.center;
+
+            doc.InsertParagraph(Environment.NewLine);
+            Paragraph letterBody = doc.InsertParagraph(letterBodyText, false, paraFormat);
+            letterBody.Alignment = Alignment.both;
+
+            doc.InsertParagraph(Environment.NewLine);
+            Paragraph letterName = doc.InsertParagraph(StudentName, false, paraFormat);
+            letterBody.Alignment = Alignment.both;
+
+            doc.InsertParagraph(Environment.NewLine);
+            doc.InsertParagraph(paraTwo, false, paraFormat);
+            doc.InsertParagraph(paraThree, false, gradeFormat);
+            //save
+            doc.Save();
 
         }
 
