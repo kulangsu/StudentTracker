39309a54bd994a9108174769ef4ba9b828ec193b
Lap To <lap_to@yahool.com>
2015-06-06 22:11:13 -0700

    test file missing, required to deployment.

---
+++ StudentTracker/test.aspx.cs
@@ -0,0 +1,42 @@
+﻿using StudentTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Configuration;
+using System.Data;
+using System.Web.Security;
+using System.Reflection;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.IO;
+
+
+
+
+namespace StudentTracker.Instructor
+{
+    public partial class UploadGradeTest : System.Web.UI.Page
+    {
+        //see IdentityModels.cs
+        StudentTrackerDBContext db = new StudentTrackerDBContext();
+        //see STLib.cs file
+        RoleManager roleManager = new RoleManager();
+
+        DataTable studentLists = new DataTable();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+  
+
+        }
+
+    }
+}
